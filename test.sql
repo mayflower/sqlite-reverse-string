ef5f256127ae0d16fb117e9d4ef193170cbf0738
Christian Speckner <christian.speckner@mayflower.de>
2015-11-20 09:30:55 +0100

    Add a unicode / BMP test.

--- test.sql
+++ test.sql
@@ -3,6 +3,7 @@ SELECT REVERSE_STRING("foo a asd asd ");
 SELECT REVERSE_STRING("fooa");
 SELECT REVERSE_STRING("foosad asdfasdf fsdf asdf asdfaf  asdfaf asdf f asdöüä üöä üöüä öüö");
 SELECT REVERSE_STRING("assd   ");
+SELECT REVERSE_STRING("الدبدوب هي لعبة على شكل دب أو أي حيوان آخر ، مصنوعة من القماش ومحشوة بالقطن. وعادة ما تعطي الأطفال شعوراً بالطمأنية والراحة. ولا عجب أن نجد بعض الكبار يحبّون الدباديب (جمع دبدوب) أيضا");
 SELECT REVERSE_STRING("𐌀𐌁𐌂");
 SELECT REVERSE_STRING("");
 SELECT REVERSE_STRING(1234);
